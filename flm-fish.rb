19c226a6a25024bb719ea0751eabed89f619b1b0
jens.paulin <jens.paulin@familjelivmedia.se>
2014-02-22 13:32:51 +0100

    added package config for flm-fish

---
+++ flm-fish.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class FlmFish < Formula
+  homepage 'https://github.com/FLM/tools'
+  url 'https://github.com/FLM/tools'
+  # sha1 '1b9574e4ba391c87f19de18bf6295b55cf298c89'
+  version '0.1'
+
+  depends_on :fishfish :vagrant
+
+  def install
+    system ""
+  end
+end
+
