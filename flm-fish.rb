75d9aab9336f231e224fea56dff59c718f53bed2
jens.paulin <jens.paulin@familjelivmedia.se>
2014-02-22 15:09:45 +0100

    update install script

--- flm-fish.rb
+++ flm-fish.rb
@@ -12,9 +12,9 @@ class FlmFish < Formula
     bin.install 'vagrant-ssh','vagrant-ssh-debug'
     etc.install 'config.fish'
     system "echo Adding 'source /usr/local/etc/config.fish' to ~/.config/fish/config if not present..."
-    system "grep -q '^source /usr/local/etc/config.fish' ~/.config/fish/config; or mkdir -p ~/.config/fish && echo 'source /usr/local/etc/config.fish' | tee -a ~/.config/fish/config"
+    system "mkdir -p ~/.config/fish && touch ~/.config/fish/config && if [ "grep -q '^source /usr/local/etc/config.fish' ~/.config/fish/config" == "" ]; then echo 'source /usr/local/etc/config.fish' | tee -a ~/.config/fish/config; fi"
     system "echo Changing default shell to fish requires your password..."
-    system "grep -q '^/usr/local/bin/fish$' /etc/shells; or echo '/usr/local/bin/fish' | sudo tee -a /etc/shells"
+    system "mkdir -p ~/.config/fish && if [ "grep -q '^/usr/local/bin/fish$' /etc/shells" == "" ]; then echo '/usr/local/bin/fish' | sudo tee -a /etc/shells; fi"
     system "chsh -s /usr/local/bin/fish"
   end
 end
