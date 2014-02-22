de590e9c609d1c8598b86b9e896eec6529a41686
jens.paulin <jens.paulin@familjelivmedia.se>
2014-02-22 14:39:18 +0100

    sha and install script fix

--- flm-fish.rb
+++ flm-fish.rb
@@ -3,13 +3,18 @@ require 'formula'
 class FlmFish < Formula
   homepage 'https://github.com/FLM/tools'
   url 'https://github.com/FLM/tools'
-  # sha1 '1b9574e4ba391c87f19de18bf6295b55cf298c89'
+  sha1 '5f00c3886a5f2602e099f7322e369215ef25cdc3'
   version '0.1'
 
   depends_on 'fishfish'
 
   def install
-    system ""
+    bin.install 'vagrant-ssh','vagrant-ssh-debug'
+    etc.install 'config.fish'
+    system "echo Adding 'source /usr/local/etc/config.fish' to ~/.config/fish/config if not present..."
+    system "grep -q '^source /usr/local/etc/config.fish' ~/.config/fish/config; or mkdir -p ~/.config/fish && echo 'source /usr/local/etc/config.fish' | tee -a ~/.config/fish/config"
+    system "echo Changing default shell to fish requires your password..."
+    system "grep -q '^/usr/local/bin/fish$' /etc/shells; or echo '/usr/local/bin/fish' | sudo tee -a /etc/shells"
+    system "chsh -s /usr/local/bin/fish"
   end
 end
-
