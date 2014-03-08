89d02685dfa2dd82a23d8716147069ac467f17ea
tomat <tomat@blarg.se>
2014-03-08 15:24:01 +0100

    fix fish default shell command

--- flm-fish.rb
+++ flm-fish.rb
@@ -14,7 +14,7 @@ class FlmFish < Formula
     system "echo Adding 'source /usr/local/etc/config.fish' to ~/.config/fish/fish.config if not present..."
     system "mkdir -p ~/.config/fish && touch ~/.config/fish/config.fish && if [ \"$(grep -q '^source /usr/local/etc/config.fish' ~/.config/fish/config.fish)\" == \"\" ]; then echo 'source /usr/local/etc/config.fish' | tee -a ~/.config/fish/config.fish; fi"
     system "echo Changing default shell to fish requires your password..."
-    system "if [ \"grep -q '^/usr/local/bin/fish$' /etc/shells\" == \"\" ]; then echo '/usr/local/bin/fish' | sudo tee -a /etc/shells; fi"
+    system "if [ \"$(grep -q '^/usr/local/bin/fish$' /etc/shells)\" == \"\" ]; then echo '/usr/local/bin/fish' | sudo tee -a /etc/shells; fi"
     system "chsh -s /usr/local/bin/fish"
   end
 end
